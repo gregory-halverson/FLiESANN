73c442db04b89390a4761dbb9fb9f7f33dcb8090
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-08-06 09:53:47 -0700

    removing hard-coded version, removing numpy version limit, organizing notebooks

--- FLiESANN/__init__.py
+++ FLiESANN/__init__.py
@@ -5,11 +5,6 @@ for the Breathing Earth Systems Simulator (BESS)
 """
 
 from .FLiESANN import *
+from .version import __version__
 
-from os.path import join, abspath, dirname
-
-with open(join(abspath(dirname(__file__)), "version.txt")) as f:
-    version = f.read()
-
-__version__ = version
 __author__ = "Gregory H. Halverson, Robert Freepartner, Hideki Kobayashi, Youngryel Ryu"
