a8a516dba84bb61dd5a684828d3ce8f8dc6f70c9
Gregory Halverson <gregory.halverson@gmail.com>
2025-10-21 15:26:46 -0700

    working on table processing

--- FLiESANN/process_FLiES_table.py
+++ FLiESANN/process_FLiES_table.py
@@ -3,6 +3,40 @@ import pandas as pd
 from .process_FLiES_ANN import FLiESANN
 
 def process_FLiES_table(FLiES_inputs_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Processes a DataFrame of FLiES inputs and returns a DataFrame with FLiES outputs.
+
+    Parameters:
+    FLiES_inputs_df (pd.DataFrame): A DataFrame containing the following columns:
+        - doy (int): Day of the year.
+        - albedo (float): Surface albedo.
+        - COT (float): Cloud optical thickness.
+        - AOT (float): Aerosol optical thickness.
+        - vapor_gccm (float): Water vapor in grams per cubic centimeter.
+        - ozone_cm (float): Ozone concentration in centimeters.
+        - elevation_km (float): Elevation in kilometers.
+        - SZA (float): Solar zenith angle in degrees.
+        - KG (str): Köppen-Geiger climate classification.
+
+    Returns:
+    pd.DataFrame: A DataFrame with the same structure as the input, but with additional columns:
+        - SWin_Wm2: incoming shortwave radiation in watts per square meter
+        - SWin_TOA_Wm2: top-of-atmosphere incoming shortwave radiation in watts per square meter
+        - UV: Ultraviolet radiation.
+        - VIS: Visible radiation.
+        - NIR: Near-infrared radiation.
+        - VISdiff: Diffuse visible radiation.
+        - NIRdiff: Diffuse near-infrared radiation.
+        - VISdir: Direct visible radiation.
+        - NIRdir: Direct near-infrared radiation.
+        - tm: Temperature.
+        - puv: Proportion of ultraviolet radiation.
+        - pvis: Proportion of visible radiation.
+        - pnir: Proportion of near-infrared radiation.
+        - fduv: Fraction of diffuse ultraviolet radiation.
+        - fdvis: Fraction of diffuse visible radiation.
+        - fdnir: Fraction of diffuse near-infrared radiation.
+    """
     FLiES_results = FLiESANN(
         day_of_year=FLiES_inputs_df.doy,
         albedo=FLiES_inputs_df.albedo,
@@ -15,7 +49,7 @@ def process_FLiES_table(FLiES_inputs_df: pd.DataFrame) -> pd.DataFrame:
         KG_climate=FLiES_inputs_df.KG
     )
 
-    Ra = FLiES_results["Ra"]
+    SWin_TOA_Wm2 = FLiES_results["SWin_TOA_Wm2"]
     SWin_Wm2 = FLiES_results["SWin_Wm2"]
     UV = FLiES_results["UV"]
     VIS = FLiES_results["VIS"]
@@ -33,7 +67,7 @@ def process_FLiES_table(FLiES_inputs_df: pd.DataFrame) -> pd.DataFrame:
     fdnir = FLiES_results["fdnir"]
 
     FLiES_outputs_df = FLiES_inputs_df.copy()
-    FLiES_outputs_df["Ra"] = Ra
+    FLiES_outputs_df["SWin_TOA_Wm2"] = SWin_TOA_Wm2
     FLiES_outputs_df["SWin_Wm2"] = SWin_Wm2
     FLiES_outputs_df["UV"] = UV
     FLiES_outputs_df["VIS"] = VIS
